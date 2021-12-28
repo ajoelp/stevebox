0230ef3f2036c8dc8123405c992bb7e05847df95
Joel Podrebarac <joelpodrebar@gmail.com>
2021-12-28 08:47:34 -0500

    Initial Commit

---
+++ Stevebox/SteveboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteveboxApp.swift
+//  Stevebox
+//
+//  Created by Joel Podrebarac on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct SteveboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
